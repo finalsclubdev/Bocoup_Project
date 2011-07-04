46016e5caddd8453c1e56ca373f331fa7d3c4d0d
adam j. sontag <ajpiano@ajpiano.com>
2011-07-03 21:36:17 -0400

    backbone 0.5 and Backbone.sync compatibility in Backbone.Store

--- client/js/libs/backbone.store.js
+++ client/js/libs/backbone.store.js
@@ -74,9 +74,18 @@
 
   });
 
+
   // Override `Backbone.sync` to use delegate to the model or collection's
   // *localStorage* property, which should be an instance of `Store`.
-  Backbone.sync = function(method, model, success, error) {
+  Backbone.sync = function(method, model, options, error) {
+
+    // Backwards compatibility with Backbone <= 0.3.3
+    if (typeof options == 'function') {
+      options = {
+        success: options,
+        error: error
+      };
+    }
 
     var resp,
         store =  model.localStorage || model.sessionStorage;
@@ -89,24 +98,16 @@
       }
 
       switch (method) {
-      case "read":
-          resp = model.id ? store.find(model) : store.findAll();
-          break;
-      case "create":
-          resp = store.create(model);
-          break;
-      case "update":
-          resp = store.update(model);
-          break;
-      case "delete":
-          resp = store.destroy(model);
-          break;
+      case "read":    resp = model.id ? store.find(model) : store.findAll(); break;
+      case "create":  resp = store.create(model);                            break;
+      case "update":  resp = store.update(model);                            break;
+      case "delete":  resp = store.destroy(model);                           break;
       }
 
       if (resp) {
-          success(resp);
+        options.success(resp);
       } else {
-          error("Record not found");
+        options.error("Record not found");
       }
   };
 
