3c28268d9c06229f4f6a38ecc482ba7a231bb2b6
Sam Bisbee <sam@sbisbee.com>
2011-07-07 18:06:02 -0400

    Can get a group by its ID from the DAO. Plus documentation.

--- server-node/GroupDAO.js
+++ server-node/GroupDAO.js
@@ -7,6 +7,25 @@ var testGroups = {
   }
 };
 
+/**
+ * Gets a map of all the groups by their id.
+ *
+ * @returns {Object} An object mapping the group ID to the group object.
+ */
 exports.getList = function() {
   return testGroups;
+};
+
+/**
+ * Gets a specific group of documents.
+ *
+ * @param {String} gid The ID of the group.
+ * @returns {Object|false} Either the object or a false value if it doesn't exist.
+ */
+exports.get = function(gid) {
+  if(!gid || typeof gid != 'string') {
+    throw 'Invalid group id.';
+  }
+
+  return (testGroups[gid]) ? testGroups[gid] : false;
 };
