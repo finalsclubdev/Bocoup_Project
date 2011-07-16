98a7fa7f4d1c631d70f35d1825b54ed2e73025d5
Sam Bisbee <sam@sbisbee.com>
2011-07-16 19:41:57 -0400

    Adding tests for CouchDBDatabaseDriver.

--- server-node/src/drivers/CouchDBDatabaseDriver.js
+++ server-node/src/drivers/CouchDBDatabaseDriver.js
@@ -1,8 +1,14 @@
+var groupValidator = require('../validators/GroupValidator.js');
+
 function makeGroupID(gid) {
   return 'group:' + gid;
 }
 
 exports.getGroup = function(gid, callback) {
+  if(!groupValidator.isValidID(gid)) {
+    throw 'Invalid gid.';
+  }
+
   if(typeof callback !== 'function') {
     throw 'Invalid callback.';
   }
