a58c3011647d3492c6a130990b7ef7bc04aac588
Sam Bisbee <sam@sbisbee.com>
2011-07-16 21:29:59 -0400

    createGroup() in the driver and unit test.

--- server-node/src/drivers/CouchDBDatabaseDriver.js
+++ server-node/src/drivers/CouchDBDatabaseDriver.js
@@ -15,3 +15,37 @@ exports.getGroup = function(gid, callback) {
 
   this.db.get(makeGroupID(gid), callback);
 };
+
+exports.createGroup = function(gid, name, callback) {
+  if(!groupValidator.isValidID(gid)) {
+    throw 'Invalid gid.';
+  }
+
+  if(!groupValidator.isValidName(name)) {
+    throw 'Invalid group name.';
+  }
+
+  gid = 'group:' + gid;
+
+  this.db.save(
+    gid,
+    {
+      name: name
+    },
+    function(err, res) {
+      if(!err) {
+        callback(
+          err,
+          {
+            _id: res._id,
+            _rev: res._rev,
+            name: name
+          }
+        );
+      }
+      else {
+        callback(err, res);
+      }
+    }
+  );
+};
